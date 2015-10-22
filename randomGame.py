de433cb54387ded0e25e401ff29bf6dafb6793d9
l7opy4uk <smoke_sev@list.ru>
2015-10-22 21:37:35 +0300

    Update randomGame.py

--- randomGame.py
+++ randomGame.py
@@ -32,27 +32,27 @@ def inputNumberFun():
 
 ## Игра по угадыванию чисел из ряда от 0 до 100.
 def randomNumberGame():
-    print("You need to guess a number in range from 0 to 100."\
-          "\nYou will have a hint every time")
+    print("\tWelcome to game \"Guess the Number\"")
+    print("\nYou need to guess a number in range from 0 to 100."\
+          "\nThere will be a hint after every try."\
+          "\nTry to do than in min. number of attempts.")
     randomNumber = random.randint(1,100)
     strLess = "It's less..."
     strMore = "It's more"
     inputNumberStr = inputEmptyDigitCheck("Enter your number: ")
     inputNumber = int(inputNumberStr)
-    count = 0
+    tries = 1
     while randomNumber != inputNumber:
         if randomNumber < inputNumber:
             print(strLess)
-            inputNumberStr = inputEmptyDigitCheck("Enter your number: ")
-            inputNumber = int(inputNumberStr)
-            count += 1
-        elif randomNumber > inputNumber:
+        else:
             print(strMore)
-            inputNumberStr = inputEmptyDigitCheck("Enter your number: ")
-            inputNumber = int(inputNumberStr)
-            count += 1
-    print("Bingo, it takes you " + str(count) + " attempts to guess a number")
-
+        inputNumberStr = inputEmptyDigitCheck("Enter your number: ")
+        inputNumber = int(inputNumberStr)
+        tries += 1
+    print("Bingo, it's really number", randomNumber)
+    print("It takes you", tries, "attempts to guess a number")
+    input("\n\nPress Enter to quit.")
 
 
 
@@ -68,7 +68,85 @@ def inputEmptyCheck(inputMessage):
 ##inputName = (inputEmptyDigitCheck("Login: ")
 def inputEmptyDigitCheck(inputMessage):
     varName = ""
+    varName = input(inputMessage)
     while not varName.isdigit():
-        varName = input("(It's must be a digit) " + inputMessage)
+            varName = input("(Use digits) " + inputMessage)
     return varName
+
+
+##                       Coin throw game
+def coingGame():
+    print("\t\tWelcome to \"Coin Throwing Game\"")
+    print("\nCoing will be throw 100 times, and will say result in the end."\
+          "\nPress any key to start.")
+    input()
+    heads = 0
+    tails = 0
+    attemptNumber = 0
+    for attemptNumber in range(100):
+        coing = random.randint(1,100)
+        if coing <= 50:
+          heads += 1
+        else:
+          tails += 1
+        attemptNumber += 1
+    print("\n\tAvarage results:"\
+          "\n\nHeads = ", heads)
+    print("\nTails = ", tails)
+    input("\nPress Enter to quit.")
+
+#Игра, вы задаете число от 1 до 100, компьютер начинает отгадывать,
+#Вы подсказываете ему, больше или меньше.
+
+def SearchRandomGame():
+    inputUser = int(inputEmptyDigitCheck("Enter your choosen number: "))
+    while inputUser > 100:
+        inputUser = int(inputEmptyDigitCheck("Enter your choosen number: "))
+    rangeRandomLower = 1
+    rangeRandomUpper = 100
+    tries = 1
+    attempt = random.randint(rangeRandomLower, rangeRandomUpper)
+    while attempt != inputUser:
+        print("\n=============================================================="\
+              "\n\nIt's more or less then", attempt,"?")
+        print("1. less"\
+              "\n2. more")
+        humanAnswer = inputEmptyDigitCheck("\nPlease enter the number 1 or 2: ")
+        
+        if inputUser < attempt:
+            while int(humanAnswer) == 2:
+                print("\n=============================================================="\
+                      "\n\nMessage from Programmer: Please don't lie to my programm"\
+                      "\n                         I watching you...")
+                print("1. less"\
+                      "\n2. more")
+                humanAnswer = inputEmptyDigitCheck("\nPlease enter the number 1 or 2: ")
+                
+            if int(humanAnswer) == 1:
+                rangeRandomUpper = attempt - 1
+        
+        elif inputUser > attempt:
+            while int(humanAnswer) == 1:
+                print("\n=============================================================="\
+                      "\n\nMessage from Programmer: Please don't lie to my programm"\
+                      "\n                         I watching you...")
+                print("1. less"\
+                      "\n2. more"\
+                      "\n==============================================================")
+                humanAnswer = inputEmptyDigitCheck("\nPlease enter the number 1 or 2: ")
+                
+            if int(humanAnswer) == 2:
+                rangeRandomLower = attempt + 1
+        else:
+            humanAnswer = inputEmptyDigitCheck("Please enter the number 1 or 2: ")
+        attempt = random.randint(rangeRandomLower, rangeRandomUpper)
+        tries += 1
+    print("\n Your number is", attempt)
+    print("\n It tooks", tries, "tries from me to guess.")     
+        
+
+
+SearchRandomGame()     
+        
+          
 
